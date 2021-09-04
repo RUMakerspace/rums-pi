4fe9bfb2689c1401c3ef79538ef68c0a138ff8a9
Jared Tulayan <jared@jaredtulayan.xyz>
2021-09-03 21:04:09 -0400

    Add default systemd-networkd configurations

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,4 +3,7 @@
 echo "${TARGET_HOSTNAME}" > "${ROOTFS_DIR}/etc/hostname"
 echo "127.0.1.1		${TARGET_HOSTNAME}" >> "${ROOTFS_DIR}/etc/hosts"
 
+install -m 644 files/00-wired.network "${ROOTFS_DIR}/etc/systemd/network/00-wired.network"
+install -m 644 files/10-wireless.network "${ROOTFS_DIR}/etc/systemd/network/10-wireless.network"
+
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
