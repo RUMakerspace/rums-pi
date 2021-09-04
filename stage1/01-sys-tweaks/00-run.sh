8250de00a76c522eaf6edc1c2b70204aaeb06c29
Jared Tulayan <jared@jaredtulayan.xyz>
2021-09-03 21:34:54 -0400

    Install oh-my-zsh on stage 1

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -e
 
+OHMYZSH="/usr/share/ohmyzsh"
+
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
+
+echo "Installing ohmyzsh..."
+on_chroot << EOF
+git clone https://github.com/ohmyzsh/ohmyzsh.git ${OHMYZSH}
+EOF
+
+install -m 644 files/zshrc "${ROOTFS_DIR}/${OHMYZSH}/zshrc"
+
+on_chroot << EOF
+ln ${OHMYZSH}/zshrc /etc/skel/.zshrc
+EOF
 
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
