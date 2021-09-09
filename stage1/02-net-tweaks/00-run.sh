fb181a5537aea84d3e2a0d886e5f72d6b5f2a8ca
Jared Tulayan <jared@jaredtulayan.xyz>
2021-09-09 10:43:16 -0400

    Try removing classic networking for systemd

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -7,3 +7,18 @@ install -m 644 files/00-wired.network "${ROOTFS_DIR}/etc/systemd/network/00-wire
 install -m 644 files/10-wireless.network "${ROOTFS_DIR}/etc/systemd/network/10-wireless.network"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
+
+echo "Uninstall classic networking..."
+on_chroot << EOF
+systemctl disable --now ifupdown dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
+apt --autoremove purge ifupdown dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
+rm -r /etc/network /etc/dhcp
+EOF
+
+echo "Setup/enable systemd-networkd and systemd-resolved..."
+on_chroot << EOF
+systemctl disable --now libnss-mdns
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+apt-mark hold avahi-daemon ifupdown dhcpcd5 isc-dhcp-client isc-dhcp-common lbnss-mdns openresolv raspberrypi-net-mods rsyslog
+systemctl enable --now systemctl-networkd systemd-resolved
+EOF
